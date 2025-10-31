00ad5401f56a86c30903c67c31e686f89ba0468e
Roman <roman.miv@yandex.ru>
2025-10-31 16:57:23 +0300

    [a] Add unbounded_queue and test

--- include/queue/unbounded_queue.hpp
+++ include/queue/unbounded_queue.hpp
@@ -1,18 +1,26 @@
 #pragma once
 #include "queue/queue.hpp"
+#include <condition_variable>
+#include <mutex>
+#include <queue>
 
 namespace dispatcher::queue {
 
 class UnboundedQueue : public IQueue {
-    // здесь ваш код
 public:
-    explicit UnboundedQueue(int capacity);
+    UnboundedQueue();
 
     void push(std::function<void()> task) override;
 
     std::optional<std::function<void()>> try_pop() override;
 
     ~UnboundedQueue() override;
+
+private:
+    std::queue<std::function<void()>> queue_;
+    std::mutex mutex_;
+    std::condition_variable not_empty_;
+    bool shutdown_;
 };
 
 }  // namespace dispatcher::queue
--- tests/queue/unbounded_queue.cpp
+++ tests/queue/unbounded_queue.cpp
@@ -2,4 +2,75 @@
 
 #include "queue/unbounded_queue.hpp"
 
-// здесь ваш код
+using namespace dispatcher::queue;
+
+TEST(UnboundedQueueTest, constructor) { EXPECT_NO_THROW(UnboundedQueue queue); }
+
+TEST(UnboundedQueueTest, pushPop) {
+    UnboundedQueue queue;
+    EXPECT_FALSE(queue.try_pop().has_value());
+
+    bool executed = false;
+    queue.push([&executed]() { executed = true; });
+
+    auto task = queue.try_pop();
+    ASSERT_TRUE(task.has_value());
+    (*task)();
+    EXPECT_TRUE(executed);
+}
+
+TEST(UnboundedQueueTest, fifo) {
+    UnboundedQueue queue;
+    std::vector<int> execution_order;
+    std::mutex order_mutex;
+
+    // Push tasks in order
+    for (int i = 0; i < 5; ++i) {
+        queue.push([i, &execution_order, &order_mutex]() {
+            std::lock_guard<std::mutex> lock(order_mutex);
+            execution_order.push_back(i);
+        });
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        auto task = queue.try_pop();
+        ASSERT_TRUE(task.has_value());
+        (*task)();
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        EXPECT_EQ(execution_order[i], i);
+    }
+}
+
+TEST(UnboundedQueueTest, capacityUnlimit) {
+    UnboundedQueue queue;
+    const int large_number = 10000;
+
+    for (int i = 0; i < large_number; ++i) {
+        queue.push([]() {});
+    }
+
+    int popped_count = 0;
+    while (auto task = queue.try_pop()) {
+        popped_count++;
+    }
+
+    EXPECT_EQ(popped_count, large_number);
+}
+
+TEST(UnboundedQueueTest, ShutdownBehavior) {
+    std::atomic<bool> push_completed{false};
+    std::thread producer;
+
+    {
+        UnboundedQueue queue;
+        producer = std::thread([&queue, &push_completed]() {
+            queue.push([]() {});
+            push_completed = true;
+        });
+        producer.join();
+        ASSERT_TRUE(push_completed.load());
+    }
+    // producer.join(); You can't do it here
+}
